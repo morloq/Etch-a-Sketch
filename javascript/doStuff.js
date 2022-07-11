42f3bc264f158a646f2b1b2ce026b299efd6be2f
Sara <sarahoubertz@gmail.com>
2022-07-11 19:13:17 +0200

    Tiles change color, corrected event-listener

--- javascript/doStuff.js
+++ javascript/doStuff.js
@@ -1,6 +1,6 @@
 //create grid using javascript:
 const container = document.querySelector(".container");
-let tr, tc;
+
 function createGrid() {
     let tbl = document.createElement('table');
     tbl.style.width = '800px';
@@ -9,30 +9,29 @@ function createGrid() {
     //16*16
     for(i = 0; i < 16; i++)
     {
-        tr = tbl.insertRow();
+        let tr = tbl.insertRow();
         tr.style.margin = 0;
         tr.style.border = "1px solid black";
         for(j = 0; j < 16; j++)
         {
-            tc = tr.insertCell();
+            let tc = tr.insertCell();
             tc.style.border = "1px solid black";
             tc.style.margin = 0;
-            tc.style.color = "red";
+            tc.style.backgroundColor = "white";
+            tc.addEventListener("mouseover", () => {
+                tc.style.backgroundColor="orange";
+            });
         }
     }
     container.appendChild(tbl);
 }
+/*
 const redButton = document.querySelector('.red');
-redButton.onclick = () => changeColor();
+//redButton.onclick = () => changeColor();
 const blackButton = document.querySelector('.black');
 const greenButton = document.querySelector('.green');
 const blueButton = document.querySelector('.blue');
 const yellowButton = document.querySelector('.yellow');
 const rainbowButton = document.querySelector('.rainbow');
-
-let containerHover = container.addEventListener("mouseover", changeColor());
-
-function changeColor(color){
-    container.style.color = "white";
-}
+*/
 createGrid();
