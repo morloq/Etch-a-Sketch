0245c850691dcf82673383ece8c18906921f33dd
Sara <sarahoubertz@gmail.com>
2022-07-11 16:24:20 +0200

    Add User interface + changeColor() (not setup)

--- javascript/doStuff.js
+++ javascript/doStuff.js
@@ -1,25 +1,38 @@
 //create grid using javascript:
+const container = document.querySelector(".container");
+let tr, tc;
 function createGrid() {
-    const container = document.querySelector(".container");
-    console.log(container);
-    var tbl = document.createElement('table');
+    let tbl = document.createElement('table');
     tbl.style.width = '800px';
     tbl.style.height = '800px';
 
     //16*16
-    for(let i = 0; i < 16; i++)
+    for(i = 0; i < 16; i++)
     {
-        var tr = tbl.insertRow();
+        tr = tbl.insertRow();
         tr.style.margin = 0;
         tr.style.border = "1px solid black";
-        for(let j = 0; j < 16; j++)
+        for(j = 0; j < 16; j++)
         {
-            var tc = tr.insertCell();
+            tc = tr.insertCell();
             tc.style.border = "1px solid black";
             tc.style.margin = 0;
+            tc.style.color = "red";
         }
     }
     container.appendChild(tbl);
 }
+const redButton = document.querySelector('.red');
+redButton.onclick = () => changeColor();
+const blackButton = document.querySelector('.black');
+const greenButton = document.querySelector('.green');
+const blueButton = document.querySelector('.blue');
+const yellowButton = document.querySelector('.yellow');
+const rainbowButton = document.querySelector('.rainbow');
 
+let containerHover = container.addEventListener("mouseover", changeColor());
+
+function changeColor(color){
+    container.style.color = "white";
+}
 createGrid();
