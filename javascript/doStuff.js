dd8660bb4ced810c530f2cd10de00db59134846e
Sara <sarahoubertz@gmail.com>
2022-07-12 11:07:03 +0200

    Input field values can be logged

--- javascript/doStuff.js
+++ javascript/doStuff.js
@@ -1,13 +1,62 @@
 //create grid using javascript:
 const container = document.querySelector(".container");
-let rows = 16;
-let cells = 16;
+let rows, cells;
+
 function createGrid() {
-    let tbl = document.createElement('table');
+    tbl = document.createElement('table');
     tbl.style.width = '800px';
     tbl.style.height = '800px';
 
     //16*16
+    for(i = 0; i < 16; i++)
+    {
+        let tr = tbl.insertRow();
+        tr.style.margin = 0;
+        tr.style.border = "1px solid black";
+        for(j = 0; j < 16; j++)
+        {
+            let tc = tr.insertCell();
+            tc.style.border = "1px solid black";
+            tc.style.margin = 0;
+            tc.style.backgroundColor = "white";
+            tc.addEventListener("mouseover", () => {
+                tc.style.backgroundColor="orange";
+            });
+        }
+    }
+    container.appendChild(tbl);
+}
+
+createGrid();
+document.querySelector(".clickMe").addEventListener("click", () => {
+    document.querySelector(".popup").style.display = "flex";
+});
+document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".popup").style.display = "none";
+})
+
+document.querySelector(".submit").addEventListener("click", updateValues());
+
+function updateValues(){
+    /*Newrows = document.querySelector(".rows").value;
+    Newcells = document.querySelector(".cells").value;*/
+    let rowsInput = document.querySelector(".rows");
+    let cellsInput = document.querySelector(".cells");
+    window.onload = function () {
+        document.querySelector(".submit").addEventListener("click", () => {
+            rows = rowsInput.value;
+            cells = cellsInput.value;
+            console.log(rows);
+            console.log(cells);
+        });
+    }
+}
+
+function eraseOldGrid(){
+    
+}
+
+function drawNewGrid(rows, cells){
     for(i = 0; i < rows; i++)
     {
         let tr = tbl.insertRow();
@@ -27,18 +76,4 @@ function createGrid() {
     container.appendChild(tbl);
 }
 
-/*const button = document.querySelector(".clickMe").addEventListener("click", () => {
-    let popup = window.open("", "enter size", "width=500, height=300");
-    
-});*/
-
-createGrid();
-document.querySelector(".clickMe").addEventListener("click", () => {
-    document.querySelector(".popup").style.display = "flex";
-});
-document.querySelector(".close").addEventListener("click", () => {
-    document.querySelector(".popup").style.display = "none";
-})
-
-let newRows = document.querySelector(".rows").value;
-console.log(newRows);
+updateValues();
